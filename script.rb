94491ffdcefe7e9ec372c209996e14228f0fbb16
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-29 02:59:09 -0700

    Add letter pick functionality

--- script.rb
+++ script.rb
@@ -1,15 +1,48 @@
 # load the dictionary
 # randomly select a word
 # store it in a variable
+module DictionaryWord
+  def PickWord
+    File.read('dictionary.txt').split(' ').select {
+      |w| w.length > 4 && w.length < 13 
+     }.sample
+  end
+end
+
+module Checkable
+
+end
+
 class Game
-  attr_accessor :secret_word
+  include DictionaryWord
+  include Checkable
+
+  attr_accessor :random_word, :hidden_word, :tries, :game_over, :letter
 
   def initialize
-    @secret_word = File.read('dictionary.txt').split(' ').select {
-       |w| w.length > 4 && w.length < 13 
-      }.sample
+    @random_word = self.PickWord
+    @hidden_word = Array.new(@random_word.length, '_')
+    @tries = 6
+    @game_over = false
+  end
+
+  def play
+    until @tries == 0 || game_over
+      puts "You have #{@tries} tries left!"
+      puts @hidden_word.join('')
+      self.input_guess
+      @tries = @tries - 1
+    end
+  end
+
+  def input_guess
+    loop do
+      puts "Enter a letter: "
+      @letter = gets.chomp
+      break if @letter.match?(/^[a-zA-Z]{1}$/)
+    end
   end
 end
 
 my_game = Game.new
-puts my_game.secret_word
+my_game.play
