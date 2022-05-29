34d468023deb8b62fece9afd4abd4fa4e6c34757
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-29 03:15:27 -0700

    Add full round of gameplay functionality

--- script.rb
+++ script.rb
@@ -5,12 +5,29 @@ module DictionaryWord
   def PickWord
     File.read('dictionary.txt').split(' ').select {
       |w| w.length > 4 && w.length < 13 
-     }.sample
+     }.sample.split('')
   end
 end
 
 module Checkable
+  def letter_check(letter, word, hidden_word)
+    word.each_with_index do |char, idx|
+      if letter == char
+        hidden_word[idx] = char
+      end
+    end
+  end
 
+  def game_over_check(word, hidden_word)
+    if word == hidden_word
+      @game_over = true
+    end
+  end
+
+  def check(letter, word, hidden_word)
+    self.letter_check(letter, word, hidden_word)
+    self.game_over_check(word, hidden_word)
+  end
 end
 
 class Game
@@ -31,18 +48,21 @@ class Game
       puts "You have #{@tries} tries left!"
       puts @hidden_word.join('')
       self.input_guess
+      self.check(@letter, @random_word, @hidden_word)
       @tries = @tries - 1
     end
+    puts "The word was #{random_word.join('')}!"
   end
 
   def input_guess
     loop do
       puts "Enter a letter: "
-      @letter = gets.chomp
+      @letter = gets.chomp  
       break if @letter.match?(/^[a-zA-Z]{1}$/)
     end
   end
 end
 
 my_game = Game.new
+print my_game.random_word
 my_game.play
