f8a2dbb637341384e8aefe7c3d9af483673b2c76
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-29 03:42:19 -0700

    Add incorrectly chosen letters display per round

--- script.rb
+++ script.rb
@@ -30,22 +30,27 @@ class Game
   include DictionaryWord
   include Checkable
 
-  attr_accessor :random_word, :hidden_word, :tries, :game_over, :letter
+  attr_accessor :random_word, :hidden_word, :tries, :game_over, :letter, :wrong_letters
 
   def initialize
     @random_word = self.PickWord
     @hidden_word = Array.new(@random_word.length, '_')
     @tries = 6
     @game_over = false
+    @wrong_letters = []
   end
 
   def play
     until @tries == 0 || game_over
       puts "You have #{@tries} tries left!"
+      puts "Wrong letter guesses: #{@wrong_letters.join(', ')}"
       puts @hidden_word.join('')
       self.input_guess
       check(@letter, @random_word, @hidden_word)
-      @tries = tries - 1 unless @random_word.any?(@letter)
+      unless @random_word.any?(@letter)
+        @tries = tries - 1
+        @wrong_letters.push(@letter)
+      end
     end
     puts "The word was #{random_word.join('')}!"
     puts @game_over ? 'You won!' : 'You lost!'
