2753f8b9614d919afce8dccfbb48961e562ec881
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-30 21:02:41 -0700

    Add comments

--- script.rb
+++ script.rb
@@ -1,5 +1,5 @@
 require 'yaml'
-
+# selects a random word between 5 and 12 letters long from a dictionary file
 module DictionaryWord
   def PickWord
     File.read('dictionary.txt').split(' ').select do |w|
@@ -9,12 +9,14 @@ module DictionaryWord
 end
 
 module Checkable
+  # checks if a players guessed letter is in the word 
+  # updates the display variable if it is
   def letter_check(letter, word, hidden_word)
     word.each_with_index do |char, idx|
       hidden_word[idx] = char if letter == char
     end
   end
-
+  # check if the words match exactly and changes game ending boolean variable
   def game_over_check(word, hidden_word)
     @game_over = true if word == hidden_word
   end
@@ -60,7 +62,8 @@ class Game
     puts "The word was #{random_word.join('')}!"
     puts @game_over ? 'You won!' : 'You lost!'
   end
-
+  # takes a single letter input
+  # gives option to save game or exit game
   def input_guess
     loop do
       puts 'Enter a letter, save or exit: '
@@ -74,7 +77,7 @@ class Game
       end
     end
   end
-
+  # stores instance variables in a yaml file
   def to_yaml
     YAML.dump ({
       :random_word => @random_word,
@@ -84,18 +87,20 @@ class Game
       :wrong_letters => @wrong_letters
     })
   end
-
+  # instantiates an object passing parameters stored in a yaml file
   def self.from_yaml(string)
     data = YAML.load string
     self.new(data[:random_word], data[:hidden_word], data[:tries], data[:game_over], data[:wrong_letters])
   end
-
+  # creates a new file in yaml form holding the current values of instance variables to be loaded later
   def save_game
     puts "Enter save game filename: "
     save_name = gets.chomp.downcase
     File.open("saves/#{save_name}.yaml", 'w') { |file| file.write(self.to_yaml) }
   end
-
+  # displays save files and takes input to load one of them in
+  # by calling the from_yaml class method and passing the inputted name equivalent
+  # to a file in the saves folder
   def load_game
     begin
     exit if Dir.children("./saves").empty?
@@ -107,7 +112,7 @@ class Game
       retry
     end
   end
-
+  # calls load game or play on input
   def start
     puts "Type 'load' to load a game, or any key to start a new game: "
     gets.chomp.match?('load') ? load_game : play
